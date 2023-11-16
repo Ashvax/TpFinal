44bbbcb5b42848725c1a04985d3520fa9560a32a
M4kyy <gonzalocuk735@gmail.com>
2023-11-15 23:52:44 -0300

    edit variables
    -

--- src/g11_pfbomberos/Vistas/Emergencias_actuales_view.java
+++ src/g11_pfbomberos/Vistas/Emergencias_actuales_view.java
@@ -33,7 +33,7 @@ public class Emergencias_actuales_view extends javax.swing.JFrame {
         jrAsignadas = new javax.swing.JRadioButton();
         jrNoAsignadas = new javax.swing.JRadioButton();
         jLabel2 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        jcEmerBrig = new javax.swing.JComboBox<>();
         jAsignar = new javax.swing.JButton();
         jDesasignar = new javax.swing.JButton();
         jResolver = new javax.swing.JButton();
@@ -104,7 +104,7 @@ public class Emergencias_actuales_view extends javax.swing.JFrame {
                                 .addGap(18, 18, 18)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(jrNoAsignadas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                                    .addComponent(jcEmerBrig, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jAsignar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addGap(34, 34, 34)
@@ -128,7 +128,7 @@ public class Emergencias_actuales_view extends javax.swing.JFrame {
                 .addGap(42, 42, 42)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jcEmerBrig, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(38, 38, 38)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jrAsignadas)
@@ -190,7 +190,6 @@ public class Emergencias_actuales_view extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jAsignar;
-    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JButton jDesasignar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -199,6 +198,7 @@ public class Emergencias_actuales_view extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JComboBox<String> jcEmerBrig;
     private javax.swing.JRadioButton jrAsignadas;
     private javax.swing.JRadioButton jrNoAsignadas;
     // End of variables declaration//GEN-END:variables
