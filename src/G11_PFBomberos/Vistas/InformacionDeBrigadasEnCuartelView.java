134657856124293766c5d9a06f16bf04d2eb7566
Ashvax <137654244+Ashvax@users.noreply.github.com>
2023-11-17 13:14:00 -0300

    Add files via upload

---
+++ src/G11_PFBomberos/Vistas/InformacionDeBrigadasEnCuartelView.java
@@ -0,0 +1,195 @@
+package bomberosApp.Vistas;
+
+import bomberosApp.AccesoADatos.BrigadaData;
+import bomberosApp.AccesoADatos.CuartelData;
+import bomberosApp.Entidades.Brigada;
+import bomberosApp.Entidades.Cuartel;
+import java.awt.Color;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+public class InformacionDeBrigadasEnCuartelView extends javax.swing.JInternalFrame {
+
+    private DefaultTableModel modelo = new DefaultTableModel();
+
+    private Cuartel cuartel = new Cuartel();
+    private CuartelData cuartelData = new CuartelData();
+    private List<Cuartel> listarCuarteles = cuartelData.ListarCuarteles();
+
+    private BrigadaData brigadaData = new BrigadaData();
+    private Brigada brigada = new Brigada();
+    private List<Brigada> listarBrigada = brigadaData.ListarBrigada();
+
+    public InformacionDeBrigadasEnCuartelView() {
+        initComponents();
+        armarCabeceras();
+        cambiarColor();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        JBBuscar = new javax.swing.JButton();
+        JBSalir = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        JTFCodigo = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        JTablaBrigadasEnCuartel = new javax.swing.JTable();
+
+        setClosable(true);
+        setIconifiable(true);
+        setMaximizable(true);
+        setTitle("Informacion de Cuartel");
+
+        JBBuscar.setFont(new java.awt.Font("Tahoma", 3, 12)); // NOI18N
+        JBBuscar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bomberosApp/Imagenes/JBBuscar.jpg"))); // NOI18N
+        JBBuscar.setText("Buscar");
+        JBBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBBuscarActionPerformed(evt);
+            }
+        });
+
+        JBSalir.setFont(new java.awt.Font("Tahoma", 3, 12)); // NOI18N
+        JBSalir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bomberosApp/Imagenes/JBSalir.png.jpg"))); // NOI18N
+        JBSalir.setText("Salir");
+        JBSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBSalirActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 3, 12)); // NOI18N
+        jLabel1.setText("Buscar Brigadas por Codigo de Cuartel");
+
+        JTablaBrigadasEnCuartel.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Id Brigada", "Nombre de la Brigada", "Especialidad", "Estado", "Id Cuartel", "Disponibilidad"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Boolean.class, java.lang.Integer.class, java.lang.Boolean.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(JTablaBrigadasEnCuartel);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(JTFCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(JBBuscar))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 814, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(18, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(JBSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JTFCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1)
+                    .addComponent(JBBuscar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(JBSalir)
+                .addGap(27, 27, 27))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void JBBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBBuscarActionPerformed
+
+        try {
+            int id = Integer.parseInt(JTFCodigo.getText());
+            List<Brigada> brigadas = brigadaData.ListarBrigadaPorCuartel(id);
+
+            if (!brigadas.isEmpty()) {
+                DefaultTableModel model = (DefaultTableModel) JTablaBrigadasEnCuartel.getModel();
+                model.setRowCount(0);
+
+                for (Brigada brigada : brigadas) {
+                    model.addRow(new Object[]{
+                        brigada.getId_brigada(),
+                        brigada.getNombre_brigada(),
+                        brigada.getEspecialidad(),
+                        brigada.isEstado(),
+                        brigada.getCuartel().getId_cuartel(),
+                        brigada.isDisponibilidad(),
+                        brigada.getNombre_cuartel()
+                    });
+                }
+
+            } else {
+                JOptionPane.showMessageDialog(this, "No se encontraron brigadas para el Cuartel con este ID.");
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(this, "Ingrese un número válido para el ID: " + e.getMessage());
+        }
+    }//GEN-LAST:event_JBBuscarActionPerformed
+//Funca
+    private void JBSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBSalirActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_JBSalirActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton JBBuscar;
+    private javax.swing.JButton JBSalir;
+    private javax.swing.JTextField JTFCodigo;
+    private javax.swing.JTable JTablaBrigadasEnCuartel;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
+
+    private void armarCabeceras() {
+        modelo.addColumn("ID Brigada");
+        modelo.addColumn("Nombre Brigada");
+        modelo.addColumn("Especialidad");
+        modelo.addColumn("Estado");
+        modelo.addColumn("ID Cuartel");
+        modelo.addColumn("Disponibilidad");
+        modelo.addColumn("Nombre de Cuartel");
+
+        JTablaBrigadasEnCuartel.setModel(modelo);
+    }
+
+    private void cambiarColor() {
+
+        this.getContentPane().setBackground(new Color(74, 84, 89));
+
+        jLabel1.setForeground(new Color(200, 214, 214));
+
+        JBBuscar.setBackground(new Color(74, 84, 89));
+        JBBuscar.setForeground(new Color(200, 214, 214));
+
+        JBSalir.setBackground(new Color(74, 84, 89));
+        JBSalir.setForeground(new Color(200, 214, 214));
+
+    }
+
+}
